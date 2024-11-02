327c5eda3b700a4e624cd5d7d796736ead87b77a
Stefanie Gay <stefanie.gay@gmail.com>
2024-11-01 21:01:38 -0400

    second commit

---
+++ Stefanie_Gay_VersionControl/Controllers/NameController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Stefanie_Gay_VersionControl.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NameController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetName()
+        {
+            return Ok(new { Name = "Stefanie Gay" });
+        }
+    }
+}
